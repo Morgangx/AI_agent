8a68f468ce02ed2fd71c84ec39636cdc558185fa
Morgangx <morgangbulawa@gmail.com>
2025-12-31 21:55:25 +0100

    Finished the project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -18,40 +19,48 @@ def main() -> None:
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args: argparse.Namespace = parser.parse_args()
     messages: list[types.Content] = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response: GenerateContentResponse = client.models.generate_content( # type: ignore
-        model="gemini-2.5-flash",
-        contents=messages,
-        config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-        )
-
-    usage_metadada: GenerateContentResponseUsageMetadata | None = response.usage_metadata
-    if usage_metadada == None:
-        raise RuntimeError("No usage metadata found!")
-    
-    function_calls: list[types.FunctionCall] | None = response.function_calls
-
-    if args.verbose:
-        print(f"User prompt: {parser}")
-        print(f"Prompt tokens: {usage_metadada.prompt_token_count}")
-        print(f"Response tokens: {usage_metadada.candidates_token_count}")
+    for _ in range(20):
+        response: GenerateContentResponse = client.models.generate_content( # type: ignore
+            model="gemini-2.5-flash",
+            contents=messages,
+            config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+            )
+        usage_metadada: GenerateContentResponseUsageMetadata | None = response.usage_metadata
+        if usage_metadada == None:
+            raise RuntimeError("No usage metadata found!")
         
-    print("Response:")
-    if function_calls is None:
-        print(response.text)
-    else:
-        function_results: list[types.Part] = []
-        for function_call in function_calls:
-            func_result: types.Content = call_function(function_call)
-            if not func_result.parts:
-                raise Exception("Function result parts don't exist")
-            func_response: types.FunctionResponse = func_result.parts[0].function_response # type: ignore
-            if not func_response:
-                raise Exception("FuntionResponse object doesn't exist")
-            if not func_response.response:
-                raise Exception("No response from the function call")
-            function_results.append(func_result.parts[0])
-            if args.verbose:
-                print(f"-> {func_result.parts[0].function_response.response}") # type: ignore
+        candidates: list[types.Candidate] | None = response.candidates
+        if candidates:
+            for cand in candidates:
+                messages.append(cand.content) # type: ignore
+
+        function_calls: list[types.FunctionCall] | None = response.function_calls
+
+        if args.verbose:
+            print(f"User prompt: {parser}")
+            print(f"Prompt tokens: {usage_metadada.prompt_token_count}")
+            print(f"Response tokens: {usage_metadada.candidates_token_count}")
+            
+        print("Response:")
+        if function_calls is None:
+            print(response.text)
+            return
+        else:
+            function_results: list[types.Part] = []
+            for function_call in function_calls:
+                func_result: types.Content = call_function(function_call)
+                if not func_result.parts:
+                    raise Exception("Function result parts don't exist")
+                func_response: types.FunctionResponse = func_result.parts[0].function_response # type: ignore
+                if not func_response:
+                    raise Exception("FuntionResponse object doesn't exist")
+                if not func_response.response:
+                    raise Exception("No response from the function call")
+                function_results.append(func_result.parts[0])
+                if args.verbose:
+                    print(f"-> {func_result.parts[0].function_response.response}") # type: ignore
+            messages.append(types.Content(role="user", parts=function_results))
+    sys.exit("Maximum number of itterations reached (20)")
 
 if __name__ == "__main__":
     main()
